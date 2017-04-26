5268e24f477184e62f44db86f6497407a63c10a6
jamie <jamie820103@gmail.com>
2017-04-26 19:16:18 +0000

    disable post script

--- bin/upload.sh
+++ bin/upload.sh
@@ -3,10 +3,8 @@ complete_dir="/downloads/complete"
 
 for entry in "$complete_dir"/*
 do
-    echo "$entry"
-    echo "$entry"
-    flock -n "$entry" -c "cd $complete_dir && drive upload -f '$entry' -p 0B6VJgbksxQNSNHRJSmRtS0ZWMXM  && rm -rf '$entry'"
-    break
+    echo "$entry uploading"
+    flock -n "$entry" -c "cd $complete_dir && drive upload -f '$entry' -p 0B6VJgbksxQNSNHRJSmRtS0ZWMXM  && rm -rf '$entry'" &
 done
 
 
